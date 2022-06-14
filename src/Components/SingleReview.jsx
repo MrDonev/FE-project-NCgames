278e28f6d0bd085a20345ba6eb12d29763424a58
Iliya <mrdonev@gmail.com>
2022-06-14 13:59:56 +0100

    one vote per page load only

--- src/Components/SingleReview.jsx
+++ src/Components/SingleReview.jsx
@@ -2,16 +2,18 @@ import { useEffect, useState } from "react";
 import { patchVote } from "../util/api";
 
 const SingleReviewTab=({review})=>{
+    const [buttonDisabled,setButtonDisabled]=useState(false)
 const [votes,setVotes]=useState(review.votes);
 
-const updateVotes=(value,id,target)=>{
+const updateVotes=(value,id)=>{
+    setButtonDisabled((isDisabled)=>!isDisabled)
   value > 0 ? setVotes((votes)=>votes+1) : setVotes((votes)=>votes-1)
   patchVote(value,id).then(({updatedReview})=>{
   }).catch((err)=>{
     value > 0 ? setVotes((votes)=>votes-1) : setVotes((votes)=>votes+1)
     alert(`There was something wrong with the data! Try again!`)
   })
-target.disabled=true;
+
 }
     return (
         <div className="ReviewCard">
@@ -30,8 +32,8 @@ target.disabled=true;
         <p>Comments: {review.comment_count}</p>
         <p>Votes {votes===0 ? review.votes : votes}</p>
         <button>View comments</button>
-        <button onClick={(e)=>updateVotes(1,review.review_id,e.target)}>⬆️ Upvote</button>
-        <button onClick={(e)=>updateVotes(-1,review.review_id,e.target)}>⬇️ Downvote</button>
+        <button disabled={buttonDisabled} onClick={()=>updateVotes(1,review.review_id)}>⬆️ Upvote</button>
+        <button disabled={buttonDisabled} onClick={()=>updateVotes(-1,review.review_id)}>⬇️ Downvote</button>
         </div>
     )
     }
